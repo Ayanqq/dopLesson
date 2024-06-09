b517eea856536beabdacd33c9a885ce61f83e747
Ayanqq <nazbievayan16@gmail.com>
2024-06-09 19:18:19 +0500

    finally ended all tasks sprint 4, 2 week

--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -1,43 +1,43 @@
 import axios from 'axios'
 
 export type GetDecksItemType = {
-  isFavorite: boolean,
-  author:{
-    id:string
-    name:string
-  }
-  id:string
-  userId:string
-  name:string
-  isPrivate:boolean
-  cover:string
-  created:string
-  updated:string
-  cardsCount:number
+    isFavorite: boolean,
+    author: {
+        id: string
+        name: string
+    }
+    id: string
+    userId: string
+    name: string
+    isPrivate: boolean
+    cover: string
+    created: string
+    updated: string
+    cardsCount: number
 }
 
 type GetDecksType = {
-  items:Array<GetDecksItemType>
-  pagination:{
-    currentPage:number,
-    itemsPerPage:number,
-    totalPages:number,
-    totalItems:number,
-  }
+    items: Array<GetDecksItemType>
+    pagination: {
+        currentPage: number,
+        itemsPerPage: number,
+        totalPages: number,
+        totalItems: number,
+    }
 }
 
 export const instance = axios.create({
-  baseURL: 'https://api.flashcards.andrii.es',
-  headers: {
-    'x-auth-skip': true,
-  },
+    baseURL: 'https://api.flashcards.andrii.es',
+    headers: {
+        'x-auth-skip': true,
+    },
 })
 
 export const decksApi = {
-  getDecks: () => {
-    return instance.get<GetDecksType>('/v2/decks')
-  },
-  postDecks: (name:string) => {
-    return instance.post('/v1/decks',{name}) // todo:в документации не написано как добавлять title - просто пустым оставить ?
-  }
+    getDecks: () => {
+        return instance.get<GetDecksType>('/v2/decks')
+    },
+    postDecks: (name: string) => {
+        return instance.post('/v1/decks', {name})
+    }
 }
--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -1,22 +1,21 @@
-import { decksApi } from './decks-api.ts'
-import { addDeckAC, setDecksAC } from './decks-reducer.ts'
-import { AppDispatch } from '../../app/store.ts'
+import {decksApi} from './decks-api.ts'
+import {addDeckAC, setDecksAC} from './decks-reducer.ts'
+import {AppDispatch} from '../../app/store.ts'
 
 export const fetchDecksTC = () => async (dispatch: AppDispatch) => {
-  try {
-    const res = await decksApi.getDecks()
-    dispatch(setDecksAC(res.data.items))
-  } catch (e) {
-    console.log(e)
-  }
+    try {
+        const res = await decksApi.getDecks()
+        dispatch(setDecksAC(res.data.items))
+    } catch (e) {
+        console.log(e)
+    }
 }
 
-export const postDecksTC = (title:string) => async (dispatch:AppDispatch) => {
-  try {
-    const res = await decksApi.postDecks(title)
-    console.log(res)
-    dispatch(addDeckAC(title, res.data))
-  } catch (e) {
-    console.log(e)
-  }
+export const postDecksTC = (title: string) => async (dispatch: AppDispatch) => {
+    try {
+        const res = await decksApi.postDecks(title)
+        dispatch(addDeckAC(title, res.data))
+    } catch (e) {
+        console.log(e)
+    }
 }
